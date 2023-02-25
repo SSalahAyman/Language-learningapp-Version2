7dd533e9b5c94ee15240ce0828199afba3a0a971
Salah Ayman <salahayman488@gmail.com>
2023-02-25 23:35:59 +0200

    splash screen and catogeryitem is finished

---
+++ languagelearningappversion2/lib/components/category_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Category extends StatelessWidget {
+  static late double width;
+  static late double height;
+  String background;
+  String text;
+  String icon;
+  // String tag;
+  VoidCallback ontap;
+
+  Category({
+    super.key,
+    required this.background,
+    required this.text,
+    required this.icon,
+    required this.ontap,
+    // required this.tag
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+        width: width * 0.8,
+        height: height * 0.09,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/photos/$background.png"),
+              fit: BoxFit.cover),
+          borderRadius: BorderRadius.circular(35),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: const TextStyle(
+                  color: Colors.white, fontSize: 30, fontFamily: 'pacifico'),
+            ),
+            SizedBox(
+              width: width * 0.04,
+            ),
+            Image.asset(
+              "assets/icons/$icon.png",
+              width: width * 0.12,
+              height: height * 0.12,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- languagelearningappversion2/lib/screens/splashscreen.dart
+++ languagelearningappversion2/lib/screens/splashscreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:get/get.dart';
 import 'package:languagelearningappversion2/screens/homepage.dart';
+
 import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -19,11 +20,63 @@ class SplashScreen extends StatelessWidget {
         child: AnimatedSplashScreen(
             splashIconSize: height,
             splash: Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(
-                          "assets/images/photos/background_Splash.png"),
-                      fit: BoxFit.cover)),
+              width: width,
+              height: height,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                        "assets/images/photos/background_Splash.png"),
+                    fit: BoxFit.cover),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    width: width,
+                    height: height * 0.6,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          height: height * 0.05,
+                        ),
+                        Image.asset(
+                          "assets/images/photos/japan.png",
+                          color: Colors.white,
+                          width: width * 0.5,
+                        ),
+                        const Text(
+                          "Toku App",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "Pacifico"),
+                        ),
+                        const Text(
+                          "Application for learning japanese",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "Pacifico"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: width,
+                    height: height * 0.4,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Lottie.asset(
+                            "assets/images/animations/130710-cara-waiting-3.json",
+                            width: width * 0.5,
+                          )
+                        ]),
+                  )
+                ],
+              ),
             ),
             nextScreen: HomePage()),
       ),
