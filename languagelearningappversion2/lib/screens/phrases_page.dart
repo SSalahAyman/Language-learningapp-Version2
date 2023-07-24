f8ef8cb106a22bc61f85ecf82bdd2a0d17c069d2
Salah Ayman <59060196+SSalahAyman@users.noreply.github.com>
2023-07-24 22:20:12 +0300

    finished Appbar of phrases page

--- languagelearningappversion2/lib/screens/phrases_page.dart
+++ languagelearningappversion2/lib/screens/phrases_page.dart
@@ -2,9 +2,36 @@ import 'package:flutter/material.dart';
 
 class PhrasesPage extends StatelessWidget {
   const PhrasesPage({super.key});
-
+  static late double width;
+  static late double height;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            SizedBox(
+              width: width * 0.18,
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: width * 0.02),
+              child: const Text(
+                "Phrases",
+                style: TextStyle(fontSize: 25, fontFamily: 'pacifico'),
+              ),
+            ),
+            Hero(
+              tag: "phrases",
+              child: Image.asset(
+                "assets/icons/phrases.png",
+                width: width * 0.1,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
